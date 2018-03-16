a264ced34018c80b96651b8065047317d3021958
Robert Falkén <f@lken.se>
2018-03-16 09:43:26 +0100

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+function App() {
+  return (
+    <div>
+      My app
+    </div>
+  )
+}
+
+export default hot(module)(App)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './app/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'app'),
+    ],
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ]
+  },
+  mode: 'development'
+}
