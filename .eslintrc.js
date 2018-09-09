8a8dfe69c3c882d26a8ce23a2b656a273ab04ad6
Robert Falkén <f@lken.se>
2018-09-09 18:12:34 +0200

    More reasonable eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,8 +25,18 @@ module.exports = {
   },
   rules: {
     semi: [2, 'never'],
-    'react/jsx-filename-extension': 0,
+    'consistent-return': 0,
+    'default-case': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': [2, { 'packageDir': path.resolve(__dirname) }],
     'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'no-case-declarations': 0,
     'no-return-assign': 0,
+    'no-unused-vars': [2, { argsIgnorePattern: '^_' }],
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-bind': 0,
   }
 }
